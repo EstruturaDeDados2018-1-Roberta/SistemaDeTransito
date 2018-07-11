1e4c292a8cc7b8657b113f4df71cc75132512542
Mathousbr <butcraft@gmail.com>
2018-07-11 10:29:44 -0300

    Edicao temporaria das funcoes do mapa

---
+++ mapa.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mapa.h"
+#include "utilitarios.h"
+
+casa* criaCasa() {
+	
+}
+
+mapa* criaMapa() {
+	mapa *novoMapa = (mapa*)malloc(sizeof(mapa));
+	int a = 0, b = 0;
+	for(a = 0; a < TAMANHO_DO_MAPA; a++) {
+		for(b = 0; b < TAMANHO_DO_MAPA; b++) {
+
+		}
+	}
+}
--- mapa.h
+++ mapa.h
@@ -11,3 +11,6 @@ struct Casa {
 struct Mapa {
 	casa *casa[TAMANHO_DO_MAPA][TAMANHO_DO_MAPA];
 }
+
+casa* criaCasa();		//Cria nova casa
+mapa* criaMapa();		//Cria o mapa inicializando todas as variaveis padronizadas
